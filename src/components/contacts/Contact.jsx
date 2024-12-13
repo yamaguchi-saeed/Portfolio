6a03a24d5ef7f97601a270792b802cbcc27cf011
Saeed Yamaguchi <42499925+yamaguchi-saeed@users.noreply.github.com>
2024-12-14 01:10:58 +0900

    changed mail address

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -58,7 +58,7 @@ const Contact = () => {
         <motion.h1 variants={variants}>Let’s work together</motion.h1>
         <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
-          <span>hankocksaeed18@gmail.com</span>
+          <span>is0637fv@ed.ritsumei.ac.jp</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Address</h2>

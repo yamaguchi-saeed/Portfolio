b0d086407fbbfc21536427a7e87644f8adf35960
Saeed Yamaguchi <42499925+yamaguchi-saeed@users.noreply.github.com>
2024-12-14 01:02:29 +0900

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,27 +5,30 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items = [
   {
     id: 1,
-    title: "React Commerce",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "React E-commerce",
+    img: "https://images.pexels.com/photos/6214479/pexels-photo-6214479.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "Developed an e-commerce platform using the Payload E-Commerce Template with Next.js, integrating features like Stripe payment processing, role-based access control, dynamic layout builder, and SEO optimizations to support digital and physical product sales.",
   },
+  
   {
     id: 2,
-    title: "Data Cleanup and Analysis in Excel",
-    img: "https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    desc: "The objective of this project was to clean and analyze a dataset in Excel to derive meaningful insights and prepare the data for further analysis or reporting.",
+    title: "Olympic Data Analysis using Python",
+    img: "https://images.pexels.com/photos/236937/pexels-photo-236937.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "Analyzed historical Olympics data to uncover trends and insights in medal distributions by country and sport. Utilized data visualization to present findings in an accessible format.",
   },
+  
   {
     id: 3,
-    title: "Vanilla JS App",
-    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "Weather App",
+    img: "https://images.pexels.com/photos/2816625/pexels-photo-2816625.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "Built a real-time weather application displaying current weather conditions using data from a public API. Features a good user experience through location-based weather updates.",
   },
+  
   {
     id: 4,
-    title: "Music App",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "Coffee Sales Data Cleanup and Analysis Using Excel",
+    img: "https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "The objective of this project was to clean and analyze a dataset in Excel to derive meaningful insights and prepare the data for further analysis or reporting.",
   },
 ];
 
